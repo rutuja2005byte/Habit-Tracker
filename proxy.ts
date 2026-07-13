1ed58c2729f379f53f891dfb577ee42469f5a1dc
Rutuja <rutujadarade2005@gmail.com>
2026-07-14 00:51:15 +0530

    deployement-3

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function proxy(request: NextRequest) {
-  return updateSession(request);
+  return await updateSession(request);
 }
 
 export const config = {
